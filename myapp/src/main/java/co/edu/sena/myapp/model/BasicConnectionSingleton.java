a4934923a24bc8e9940a3a305930ce93cceba8c7
JuanElKantar <128154436+JuanElKantar@users.noreply.github.com>
2023-05-05 22:27:10 -0500

    Add files via upload

---
+++ myapp/src/main/java/co/edu/sena/myapp/model/BasicConnectionSingleton.java
@@ -0,0 +1,19 @@
+package co.edu.sena.myapp.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BasicConnectionSingleton {
+    private static String url = "jdbc:mysql://localhost:3306/myapp?serverTimezone=America/Bogota";
+    private static String user = "juan1";
+    private static String pass = "3169648Aa";
+    private static Connection conn;
+    public static Connection getInstance()
+            throws SQLException {
+        if (conn == null) {
+            conn = DriverManager.getConnection(url, user, pass);
+        }
+        return conn;
+    }
+} // BasicConnectionSingleton
