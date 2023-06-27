23827ddd34710fa64651fa8dc572ecf16b753a16
JuanElKantar <128154436+JuanElKantar@users.noreply.github.com>
2023-06-27 08:23:00 -0500

    Add files via upload

---
+++ myapp/src/main/java/co/edu/sena/myapp/model/BasicConnection.java
@@ -0,0 +1,37 @@
+package co.edu.sena.myapp.model;
+
+import java.sql.*;
+
+public class BasicConnection {
+    public static void main(String[] args)
+    {
+        String url ="jdbc:mysql://localhost:3306/myapp?serverTimezone=America/Bogota";
+            String username = "juan1";
+        String password = "3169648Aa";
+        String sql = null;
+        Connection conn = null;
+        Statement stm = null;
+        ResultSet rs = null;
+        try {
+            sql = "SELECT * FROM myapp.users_tbl";
+            conn = DriverManager.getConnection(url, username, password);
+            stm = conn.createStatement();
+            rs = stm.executeQuery(sql);
+            while (rs.next()) {
+                System.out.print(rs.getString("user_firstname"));
+                System.out.print(" | ");
+                System.out.println(rs.getString("user_lastname"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                rs.close();
+                stm.close();
+                conn.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }// end try-catch
+    } // main
+} // BasicConnection
